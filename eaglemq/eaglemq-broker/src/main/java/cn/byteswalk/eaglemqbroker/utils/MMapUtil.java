61a2d17dac9ae6ba3163526b100f9449771cddbe
haosh <haoshichuan@foxmail.com>
2024-08-21 17:25:40 +0800

    mmap 工具测试

--- eaglemq/eaglemq-broker/src/main/java/cn/byteswalk/eaglemqbroker/utils/MMapUtil.java
+++ eaglemq/eaglemq-broker/src/main/java/cn/byteswalk/eaglemqbroker/utils/MMapUtil.java
@@ -14,13 +14,16 @@ import java.nio.MappedByteBuffer;
 
 import java.nio.channels.FileChannel;
 
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 /**
  * @Author hsc
  * @Date Create in 15:09 2024/7/23
- * @Description 支持基于 Java 的 MMap api 访问文件能力（文件的学写的能力）
+ * @Description 支持基于 Java 的 MMap api 访问文件能力（文件的读写的能力）
+ * <p>
  * <p>
  * 支持指定的 offset 的文件映射（结束映射的 offset-开始映射的 offset = 映射的内存体积） done!
  * 文件从指定的 offset 开始读取 done!
@@ -29,17 +32,20 @@ import java.util.concurrent.TimeUnit;
  */
 public class MMapUtil {
 
+    private static final String RW_ACCESS_MODE = "rw";
+
     private File file;
-    private int mappedSize;
-    private MappedByteBuffer mbb;
-    private FileChannel fc;
+    // MappedByteBuffer 是 DirectByteBuffer，即直接内存
+    // DirectByteBuffer 特点之一是不受限制于 JVM 的 GC 回收的管理，意味着将这个对象设置为 null
+    private MappedByteBuffer mappedByteBuffer;
+    private FileChannel fileChannel;
 
     /**
      * 指定offset做文件映射
      *
      * @param filePath    文件路径
      * @param startOffset 开始映射的offset
-     * @param mappedSize  映射的体积
+     * @param mappedSize  映射的体积（endOffset-startOffset）
      */
     public void loadFileInMMap(String filePath, int startOffset, int mappedSize)
             throws IOException {
@@ -47,25 +53,30 @@ public class MMapUtil {
         if (!file.exists()) {
             throw new FileNotFoundException("filePath is " + filePath + " inValid");
         }
-        this.fc = new RandomAccessFile(file, "rw").getChannel();
-        this.mbb = this.fc.map(FileChannel.MapMode.READ_WRITE, startOffset, mappedSize);
-        System.out.println("------");
+        // noinspection resource
+        this.fileChannel = new RandomAccessFile(file, RW_ACCESS_MODE).getChannel();
+        // MapMode:映射模式|position:开始读的位置|size:读取内容的大小
+        if (mappedSize > Integer.MAX_VALUE) {
+            throw new IllegalArgumentException("Size exceeds Integer.MAX_VALUE");
+        }
+        this.mappedByteBuffer = this.fileChannel.map(FileChannel.MapMode.READ_WRITE, startOffset, mappedSize);
     }
+
 
     /**
      * 支持从文件从指定的 offset 开始读取内容
      *
-     * @param readOffset
-     * @param size
-     * @return
+     * @param readOffset 读取内容的起始位置
+     * @param size       读取内容的大小
+     * @return 返回读取内容
      */
     public byte[] readContent(int readOffset, int size) {
-        mbb.position(readOffset);
+        mappedByteBuffer.position(readOffset);
         byte[] content = new byte[size];
         int j = 0;
         for (int i = 0; i < size; i++) {
             // 从内存里访问，快速高效，不用担心
-            byte b = mbb.get(readOffset + i);
+            byte b = mappedByteBuffer.get(readOffset + i);
             content[j++] = b;
         }
         return content;
@@ -73,6 +84,7 @@ public class MMapUtil {
 
     /**
      * 更高性能的一种写入api
+     *
      * @param content 内容
      */
     public void writeContent(byte[] content) {
@@ -83,67 +95,47 @@ public class MMapUtil {
      * 写入数据到磁盘中
      *
      * @param content 内容
-     * @param force 强制刷盘标志
+     * @param force   强制刷盘标志
      */
     public void writeContent(byte[] content, boolean force) {
         // 默认刷到 page cache 中
         // 如果需要强制刷盘，这里要兼容
-        mbb.put(content);
+        mappedByteBuffer.put(content);
         if (force) {
             // 强制刷盘
-            mbb.force();
+            mappedByteBuffer.force();
         }
     }
-
-    private void clear() throws IOException {
-        mbb.clear();
-        // 在关闭资源的时候执行以下代码释放内存
-        // 不推荐的原因是因为使用了 sun包下不稳定的代码
-//        Method method = FileChannelImpl.class.getDeclaredAnnotation("unmap", MappedByteBuffer.class);
-//        method.setAccessible(true);
-//        method.invoke(FileChannelImple.class, mbb);
-        fc.close();
-    }
-
-
-
-    public void cleaner() {
-        try {
-            Method cleanerMethod = mbb.getClass().getMethod("cleaner");
-            cleanerMethod.setAccessible(true);
-            Object cleaner = cleanerMethod.invoke(mbb);
-            Method cleanMethod = cleaner.getClass().getMethod("clean");
-            cleanMethod.setAccessible(true);
-            cleanMethod.invoke(cleaner);
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to clean the MappedByteBuffer", e);
-        }
-
-    }
-
 
     public void clean() {
-        if (mbb == null || !mbb.isDirect() || mbb.capacity() == 0) {
+        if (mappedByteBuffer == null || !mappedByteBuffer.isDirect() || mappedByteBuffer.capacity() == 0) {
             return;
         }
-        invoke(invoke(viewed(mbb), "cleaner"), "clean");
+        invoke(invoke(viewed(mappedByteBuffer), "cleaner"), "clean");
     }
 
+    // AccessController.doPrivileged 在 jdk17 已经过时
     private Object invoke(final Object target, final String methodName, final Class<?>... args) {
-        try {
-            Method method = method(target, methodName, args);
-            method.setAccessible(true);
-            return method.invoke(target);
-        } catch (IllegalAccessException | InvocationTargetException e) {
-            throw new RuntimeException(e);
-        }
+        return AccessController.doPrivileged(new PrivilegedAction<Object>() {
+            @Override
+            public Object run() {
+                try {
+                    Method method = method(target, methodName, args);
+                    method.setAccessible(true);
+                    return method.invoke(target);
+                } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
     }
 
-    private Method method(Object target, String methodName, Class<?>... args) {
+    private Method method(Object target, String methodName, Class<?>[] args)
+            throws NoSuchMethodException {
         try {
             return target.getClass().getMethod(methodName, args);
         } catch (NoSuchMethodException e) {
-            throw new RuntimeException(e);
+            return target.getClass().getDeclaredMethod(methodName, args);
         }
     }
 
@@ -171,57 +163,15 @@ public class MMapUtil {
 //        mMapUtil.loadFileInMMap("E:\\bytewalk\\jproject\\eagle\\eaglemq\\broker\\store\\order_cancel_topic\\00000000", 0, 1024*1024*1);
         Scanner input = new Scanner(System.in);
         int i = 0;
-        long size = input.nextLong();
+        int size = input.nextInt();
         MMapUtil mMapUtil = new MMapUtil();
+        String file = "E:\\bytewalk\\jproject\\eagle\\eaglemq\\broker\\store\\order_cancel_topic\\00000000";
         //默认是字节
-        mMapUtil.loadFileInMMap("E:\\bytewalk\\jproject\\eagle\\eaglemq\\broker\\store\\order_cancel_topic\\00000000", 0, (int) (1024 * 1024 * size));
-        System.out.println("映射了" + size + "m的空间");
-        TimeUnit.SECONDS.sleep(5);
+        mMapUtil.loadFileInMMap(file, 0, 1024 * 1024 * size);
+        System.out.println("映射了" + size + " m 的空间");
+        TimeUnit.SECONDS.sleep(5);;
+        mMapUtil.clean();
         System.out.println("释放内存");
-//        mMapUtil.clean();
-        mMapUtil.cleaner();
         TimeUnit.SECONDS.sleep(10000);
     }
-
-//    public void clean() {
-//        if (mbb == null || !mbb.isDirect() || mbb.capacity() == 0) {
-//            return;
-//        }
-//        invoke(invoke(viewed(mbb), "cleaner"), "clean");
-//    }
-
-// AccessController.doPrivileged 在 jdk17 已经过时
-//    private Object invoke(final Object target, final  String methodName, final Class<?>... args) {
-//        return AccessController.doPrivileged(new PrivilegedAction<Object>() {
-//            @Override
-//            public Object run() {
-//
-//                try {
-//                    Method method = method(target, methodName, args);
-//                    method.setAccessible(true);
-//                    return method.invoke(target);
-//                } catch (IllegalAccessException | InvocationTargetException e) {
-//                    throw new RuntimeException(e);
-//                }
-//            }
-//        });
-//    }
-
-//    private ByteBuffer viewed(ByteBuffer buffer) {
-//        String methodName = "viewedBuffer";
-//        Method[] methods = buffer.getClass().getMethods();
-//        for (Method method : methods) {
-//            if (method.getName().equals("attachment")) {
-//                methodName = "attachment";
-//                break;
-//            }
-//        }
-//
-//        ByteBuffer viewedBuffer = (ByteBuffer) invoke(buffer, methodName);
-//        if (viewedBuffer == null) {
-//            return buffer;
-//        } else {
-//            return viewed(viewedBuffer);
-//        }
-//    }
 }
