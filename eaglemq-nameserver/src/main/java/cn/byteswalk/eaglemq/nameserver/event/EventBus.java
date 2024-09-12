fe3ed2e47e3a7e9c396b68cc8158dda64cd75111
haosh <haoshichuan@foxmail.com>
2024-09-12 18:21:16 +0800

    1. 修改文件名称

--- eaglemq-nameserver/src/main/java/cn/byteswalk/eaglemq/nameserver/event/EventBus.java
+++ eaglemq-nameserver/src/main/java/cn/byteswalk/eaglemq/nameserver/event/EventBus.java
@@ -83,6 +83,12 @@ public class EventBus {
     }
 
     public static void main(String[] args) {
+//        ServiceLoader<Listener> serviceLoader = ServiceLoader.load(Listener.class);
+//        for (Listener listener : serviceLoader) {
+//            System.out.println(listener.getClass().getName());
+//        }
+        EventBus eventBus = new EventBus();
+        eventBus.init();
     }
 }
 
